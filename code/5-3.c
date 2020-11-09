63e45bd6d8d7537113e808fff8aefe52c13d9c24
hualiang123 <2027792537@qq.com>
2020-11-09 14:17:59 +0800

    week7

--- code/5-3.c
+++
@@ -1,7 +0,0 @@
-#include"ch05.h"
-int main(){
-setvbuf(stdout,NULL,_IONBF,0);
-printf("hello world!");
-while(1);
-return 0;
-}
--- code/5-4.c
+++
@@ -1,7 +0,0 @@
-#include"ch05.h"
-int main(){
-printf("hello world!");
-fflush(stdout);
-while(1);
-return 0;
-}
